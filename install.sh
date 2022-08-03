32f5ec6ad4210c224089e4ae219e51c053fce09c
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-08-03 13:02:18 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+helm repo add bitnami https://charts.bitnami.com/bitnami
 helm repo add skm https://charts.sagikazarmark.dev
 
-NAMESPACE="log-archiver"
+helm install --create-namespace --namespace nginx-ingress-controller --wait --values nginx-ingress-controller.values.yaml nginx-ingress-controller bitnami/nginx-ingress-controller
 
+
+NAMESPACE="log-archiver"
 helm install --create-namespace --namespace "$NAMESPACE" --values ./sftpgo.values.yaml --wait "$NAMESPACE" skm/sftpgo
+
+
+# helm repo add bitnami https://charts.bitnami.com/bitnami
+# helm install --create-namespace -namespace nginx-ingress-controller --wait --values nginx-ingress-controller.values.yaml nginx-ingress-controller bitnami/nginx-ingress-controller
