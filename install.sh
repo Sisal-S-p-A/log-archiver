dca5ff437f2851fd3548c11d8aa934b796349485
Alberto Clemente <70150664+Albe83@users.noreply.github.com>
2022-08-03 11:38:58 +0200

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+helm repo add skm https://charts.sagikazarmark.dev
+
+NAMESPACE="log-archiver"
+
+helm install \
+  --create-namespace \
+  --namespace "$NAMESPACE" \
+  --values ./sftpgo.values.yaml \
+  "$NAMESPACE" \
+  skm/sftpgo
